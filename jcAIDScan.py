9afb5817a7f841c4087257ac5bbb6de581d485ac
petrs <petrsgit@gmail.com>
2018-07-19 19:58:21 +0200

    refactoring, comments, performance measurement

--- jcAIDScan.py
+++ jcAIDScan.py
@@ -4,12 +4,11 @@ import subprocess
 from shutil import copyfile
 import time
 
-# TODO: save complete log to separate named file
-# Format Import.cap: 04 00 len num_packages package1 package2 ... packageN
-# packageFormat: package_major package_minor package_len AID
-
 SCRIPT_VERSION = '0.1.1'
 BASE_PATH = '.'
+
+FORCE_UNINSTALL = True
+
 GP_BASIC_COMMAND = 'gp.exe'
 GP_AUTH_FLAG = ''  # most of the card requires no additional authentication flag
 # GP_AUTH_FLAG = '--emv'  # use of EMV key diversification is used (e.g., G&D cards)
@@ -87,6 +86,7 @@ class PackageAID:
 
     def serialize(self):
         aid_str = ''.join('{:02X}'.format(a) for a in self.aid)
+        # one package format: package_major package_minor package_len package_AID
         serialized = '{:02X}{:02X}{:02X}{}'.format(self.minor, self.major, len(self.aid), aid_str)
         return serialized
 
@@ -201,35 +201,39 @@ java_lang = PackageAID(b'\xA0\x00\x00\x00\x62\x00\x01', 1, 0)
 package_template = b'\xA0\x00\x00\x00\x62\x01\x01'
 
 
-def check(import_section, package, uninstall):
+def check_aid(import_section, package, uninstall):
+    # save content of Import.cap into directory structure
     print(import_section)
     f = open('{0}\\template\\test\\javacard\\Import.cap'.format(BASE_PATH), 'wb')
     f.write(bytes.fromhex(import_section))
     f.close()
 
-    # create new
+    # create new cap file by zip of directories
     shutil.make_archive('test.cap', 'zip', '{0}\\template\\'.format(BASE_PATH))
 
     package_hex = package.serialize()
 
-    # remove zip appendix
+    # remove zip suffix
     os.remove('test.cap')
     os.rename('test.cap.zip', 'test.cap')
+    # store used cap file
     copyfile('test.cap', '{0}\\results\\test_{1}.cap'.format(BASE_PATH, package_hex))
 
-    uninstall = True
-    if uninstall:
+    # (try to) uninstall previous applet if necessary/required
+    if uninstall or FORCE_UNINSTALL:
         subprocess.run([GP_BASIC_COMMAND, GP_AUTH_FLAG, '--uninstall', 'test.cap'], stdout=subprocess.PIPE)
 
-    # try to install
+    # try to install test applet
     result = subprocess.run([GP_BASIC_COMMAND, GP_AUTH_FLAG, '--install', 'test.cap', '--d'], stdout=subprocess.PIPE)
 
+    # store gp result into log file
     result = result.stdout.decode("utf-8")
-    # print(result)
     f = open('{0}\\results\\{1}.txt'.format(BASE_PATH, package_hex), 'w')
     f.write(result)
     f.close()
 
+    # heuristics to detect successful installation - log must contain error code 0x9000 followed by SCardEndTransaction
+    # If installation fails, different error code is present
     if result.find('9000\r\nSCardEndTransaction()') != -1:
         return True
     else:
@@ -237,44 +241,21 @@ def check(import_section, package, uninstall):
 
 
 def format_import(packages_list):
-    total_len = 0
+    total_len = 1 # include count of number of packages
     for package in packages_list:
         total_len += package.get_length()
-    total_len += 1 # include count of number of packages
+
+    # format of Import.cap: 04 00 len num_packages package1 package2 ... packageN
     import_section = '0400{:02x}{:02x}'.format(total_len, len(packages_list))
+
+    # serialize all packages
     for package in packages_list:
         import_section += package.serialize()
 
     return import_section
 
 
-def test():
-    javacardx_biometry1toN = PackageAID(b'\xA0\x00\x00\x00\x62\x02\x04', 1, 0)
-    javacard_framework = PackageAID(b'\xA0\x00\x00\x00\x62\x01\x01', 1, 3)
-    javacard_security = PackageAID(b'\xA0\x00\x00\x00\x62\x01\x02', 1, 3)
-    javacardx_crypto = PackageAID(b'\xA0\x00\x00\x00\x62\x02\x01', 1, 3)
-    java_lang = PackageAID(b'\xA0\x00\x00\x00\x62\x00\x01', 1, 0)
-    java_lang = PackageAID(b'\xA0\x00\x00\x00\x62\x00\x01', 1, 0)
-    imported_packages = []
-
-    imported_packages.append(java_lang)
-    imported_packages.append(javacard_security)
-    imported_packages.append(javacard_framework)
-    imported_packages.append(javacardx_biometry1toN)
-
-    import_content = format_import(imported_packages)
-    check(import_content, javacardx_biometry1toN, True)
-
-    supported = []
-    tested = {}
-    run_scan(TestCfg(1, 1, 0, 0, "a0000000620204"), supported, tested)
-    card_info = get_card_info("test")
-    save_scan(card_info, supported, tested)
-
-
-
-
-def test_aid(tested_package_aid, is_installed, supported_list, tested):
+def test_aid(tested_package_aid, is_installed, supported_list, tested_list):
     imported_packages = []
     imported_packages.append(javacard_framework)
     # do not import java_lang as default (some cards will then fail to load)
@@ -283,15 +264,15 @@ def test_aid(tested_package_aid, is_installed, supported_list, tested):
 
     import_content = format_import(imported_packages)
 
-    if check(import_content, tested_package_aid, is_installed):
+    if check_aid(import_content, tested_package_aid, is_installed):
         print(" ###########\n  {0} IS SUPPORTED\n ###########\n".format(tested_package_aid.get_readable_string()))
         supported_list.append(tested_package_aid)
-        tested[tested_package_aid] = True
+        tested_list[tested_package_aid] = True
         is_installed = True
     else:
         print("   {0} v{1}.{2} is NOT supported ".format(tested_package_aid.get_aid_hex(), tested_package_aid.major,
                                                          tested_package_aid.minor))
-        tested[tested_package_aid] = False
+        tested_list[tested_package_aid] = False
         is_installed = False
 
     return is_installed
@@ -316,23 +297,27 @@ def run_scan(cfg, supported, tested):
     localtime = time.asctime(time.localtime(time.time()))
     print(localtime)
 
-    template = bytearray(bytes.fromhex(cfg.package_template))
-    is_installed = True
-    # now check all possible values
+    # start performance measurements
+    elapsed = -time.perf_counter()
+    num_tests = 0
+
+    is_installed = True # assume that test applet was installed to call uninstall
+
+    # check all possible values from specified ranges
     for major in range(cfg.min_major, cfg.max_major + 1):
         print("############################################\n")
-        print("MAJOR = {0}".format(major))
+        print("MAJOR = {0:02X}".format(major))
         print_supported(supported)
         for minor in range(cfg.min_minor, cfg.max_minor + 1):
             print_supported(supported)
-            print("############################################\n")
-            print("MAJOR = {0}, MINOR = {1}".format(major, minor))
+            print("###########################:#################\n")
+            print("MAJOR = {0:02X}, MINOR = {1:02X}".format(major, minor))
             # compute start and end of required range
             [start4, stop4] = cfg.get_val4_range()
             for val_4 in range(start4, stop4 + 1):
                 print_supported(supported)
                 print("############################################\n")
-                print("MAJOR = {0}, MINOR = {1}, VAL4 = {2}".format(major, minor, val_4))
+                print("MAJOR = {0:02X}, MINOR = {1:02X}, VAL4 = {2:02X}".format(major, minor, val_4))
                 # compute start and end of required range
                 [start5, stop5] = cfg.get_val5_range()
                 for val_5 in range(start5, stop5 + 1):
@@ -344,13 +329,16 @@ def run_scan(cfg, supported, tested):
                         new_package_aid[5] = val_5
                         new_package_aid[6] = val_6
                         new_package = PackageAID(new_package_aid, major, minor)
-
+                        # test current package
                         is_installed = test_aid(new_package, is_installed, supported, tested)
+                        num_tests += 1
+
+    # end performance measurements
+    elapsed += time.perf_counter()
 
     print_supported(supported)
 
-    localtime = time.asctime(time.localtime(time.time()))
-    print(localtime)
+    print("Elapsed time: {0:0.2f}s, {1:0.2f} AIDs / min\n".format(elapsed, num_tests / (elapsed / 60)))
 
     print("################# END ###########################\n")
     print(cfg)
@@ -478,7 +466,10 @@ def main():
     # scan standard JC API
     supported = []
     tested = {}
+    elapsed = -time.perf_counter()
     scan_JC_API_305(card_info, supported, tested)
+    elapsed += time.perf_counter()
+    print("Complete test elapsed time: {0:0.2f}s\n".format(elapsed))
     # create file with results
     save_scan(card_info, supported, tested)
 
@@ -516,3 +507,26 @@ def scan_subpackages(supported):
     # print all results
     print_supported(supported)
     print_supported(supported_01)
+
+def test():
+    javacardx_biometry1toN = PackageAID(b'\xA0\x00\x00\x00\x62\x02\x04', 1, 0)
+    javacard_framework = PackageAID(b'\xA0\x00\x00\x00\x62\x01\x01', 1, 3)
+    javacard_security = PackageAID(b'\xA0\x00\x00\x00\x62\x01\x02', 1, 3)
+    javacardx_crypto = PackageAID(b'\xA0\x00\x00\x00\x62\x02\x01', 1, 3)
+    java_lang = PackageAID(b'\xA0\x00\x00\x00\x62\x00\x01', 1, 0)
+    java_lang = PackageAID(b'\xA0\x00\x00\x00\x62\x00\x01', 1, 0)
+    imported_packages = []
+
+    imported_packages.append(java_lang)
+    imported_packages.append(javacard_security)
+    imported_packages.append(javacard_framework)
+    imported_packages.append(javacardx_biometry1toN)
+
+    import_content = format_import(imported_packages)
+    check_package_aid(import_content, javacardx_biometry1toN, True)
+
+    supported = []
+    tested = {}
+    run_scan(TestCfg(1, 1, 0, 0, "a0000000620204"), supported, tested)
+    card_info = get_card_info("test")
+    save_scan(card_info, supported, tested)
