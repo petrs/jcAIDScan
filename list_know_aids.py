1e3db8eccee1db1a0a575a7641fb16976ef776df
petrs <petrsgit@gmail.com>
2018-07-21 23:19:15 +0200

    print RID info

--- list_know_aids.py
+++ list_know_aids.py
@@ -7,11 +7,20 @@ GP_AUTH_FLAG = ''  # most of the card requires no additional authentication flag
 # GP_AUTH_FLAG = '--emv'  # use of EMV key diversification is used (e.g., G&D cards)
 
 
-def best_match(aid, aid_list):
+def best_match_rid(aid, rid_list):
+    for rid_ctx in rid_list:
+        if len(rid_ctx) > 0:
+            if aid.startswith(rid_ctx[0]):
+                return "{0}, Vendor: {1} / {2}".format(rid_ctx[0], rid_ctx[1], rid_ctx[2])
+
+    return "unknown RID"
+
+
+def best_match_aid(aid, aid_list):
     for aid_ctx in aid_list:
         if len(aid_ctx) > 0:
             if aid == aid_ctx[0]:
-                return "{0}, {1}, Vendor: {2}/{3}".format(aid_ctx[3], aid_ctx[4], aid_ctx[1], aid_ctx[2])
+                return "{0}, {1}, Vendor: {2} / {3}".format(aid_ctx[3], aid_ctx[4], aid_ctx[1], aid_ctx[2])
 
     return "unknown AID"
 
@@ -32,6 +41,17 @@ def gp_list():
             aid_ctx[3] = aid_ctx[3].strip()
             aid_ctx[4] = aid_ctx[4].strip()
 
+    with open('well_known_rids.csv', 'r') as f:
+        reader = csv.reader(f)
+        rid_list = list(reader)
+
+    # strip leading/trailing zeroes
+    for rid_ctx in rid_list:
+        if len(rid_ctx) >= 3:
+            rid_ctx[0] = rid_ctx[0].strip()
+            rid_ctx[1] = rid_ctx[1].strip()
+            rid_ctx[2] = rid_ctx[2].strip()
+
     # run gp and get list of applets
     result = subprocess.run([GP_BASIC_COMMAND, GP_AUTH_FLAG, '--list'], stdout=subprocess.PIPE)
     result_text = result.stdout.decode("utf-8")
@@ -44,7 +64,8 @@ def gp_list():
             # AID in output detected
             print(line)
             # annotate (if known)
-            print("AID info: {0}".format(best_match(match.group("aid"), aid_list)))
+            print("     RID info: {0}".format(best_match_rid(match.group("aid"), rid_list)))
+            print("     AID info: {0}".format(best_match_aid(match.group("aid"), aid_list)))
         else:
             # other lines from gp - just print
             print(line)
